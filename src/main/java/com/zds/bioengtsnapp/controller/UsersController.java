25c259960ffd07eeee85309057b1b2346ef96c2a
郑笃实 <3388214266@qq.com>
2026-01-04 22:03:42 +0800

    build: Update WAR package

--- src/main/java/com/zds/bioengtsnapp/controller/UsersController.java
+++ src/main/java/com/zds/bioengtsnapp/controller/UsersController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("users")
 public class UsersController {
 
     private final UsersService usersService;
