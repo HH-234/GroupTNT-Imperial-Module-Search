31010c6ff714872e67f3a8d921260e95fd5f8872
郑笃实 <3388214266@qq.com>
2026-01-05 00:55:03 +0800

    123

--- src/main/java/com/zds/bioengtsnapp/controller/UsersController.java
+++ src/main/java/com/zds/bioengtsnapp/controller/UsersController.java
@@ -3,7 +3,10 @@ package com.zds.bioengtsnapp.controller;
 import com.zds.bioengtsnapp.domain.Users;
 import com.zds.bioengtsnapp.dto.UserDetailDTO;
 import com.zds.bioengtsnapp.service.UsersService;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
@@ -17,11 +20,19 @@ public class UsersController {
 
     /**
      * 根据全名搜索用户基本信息
-     * @param fullName 用户全名
-     * @return 用户基本信息列表
+     * URL: /users/search?fullName=xxx
      */
     @GetMapping("/users/search")
     public List<Users> searchByFullName(@RequestParam String fullName) {
         return usersService.getUsersByFullName(fullName);
+    }
+
+    /**
+     * 根据全名搜索用户详细信息（包含 phoneNumbers / addresses / discoveryUrlId 等）
+     * URL: /users/search/details?fullName=xxx
+     */
+    @GetMapping("/users/search/details")
+    public List<UserDetailDTO> searchDetailsByFullName(@RequestParam String fullName) {
+        return usersService.getUserDetailsByFullName(fullName);
     }
 }
