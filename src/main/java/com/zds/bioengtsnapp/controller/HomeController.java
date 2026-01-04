11c5e65375804008813cc9b6e681185ca1aa595c
郑笃实 <3388214266@qq.com>
2026-01-04 23:03:04 +0800

    update: rebuild and deploy

--- src/main/java/com/zds/bioengtsnapp/controller/HomeController.java
+++ src/main/java/com/zds/bioengtsnapp/controller/HomeController.java
@@ -3,14 +3,11 @@ package com.zds.bioengtsnapp.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class HomeController {
+// @Controller
+// public class HomeController {
 
-    @GetMapping("")
-    public String home() {
-        return "forward:/index.html";
-    }
-}
-
-
-
+//     @GetMapping("")
+//     public String home() {
+//         return "forward:/index.html";
+//     }
+// }
